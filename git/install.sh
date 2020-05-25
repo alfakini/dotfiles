0bd03803bdd56bcf712e78eddd0daaa3e6ec493b
Alan Fachini <hi@alfakini.com>
2020-05-25 10:58:23 -0300

    Update git install and config

--- git/install.sh
+++ git/install.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
+if brew ls --versions git-lfs > /dev/null; then
+  echo "[INFO] Skipped, git-lfs already installed"
+else
+  echo "[INFO] Installing git-lfs"
+  brew install git-lfs
+fi
+
 git lfs install
 git lfs install --system
